8d52cd804346e925cfe172fb860f140117737110
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2023-11-29 09:50:05 -0300

    update projeto bateria

--- projetos front-end/aluramidi-curso-arquivos-iniciais/main.js
+++ projetos front-end/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,35 @@
-alert('Hello world');
+
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+    
+    
+}
+
+
+
+const arrayDeTeclas = document.querySelectorAll('.tecla');
+
+let i = 0;
+
+//enquanto
+while(i < arrayDeTeclas.length){
+
+    const tecla = arrayDeTeclas[i];
+
+    const instrumentos = tecla.classList[1];
+
+    const idAudio = `#som_${instrumentos}`;
+    
+       tecla.onclick = function (){
+        tocaSom(idAudio);
+       }
+
+        i++;
+
+       
+    
+}
+
+
+
+
