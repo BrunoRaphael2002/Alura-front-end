7b8f10d80e2a1d40387021408cdeb0e1c923d581
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2023-11-29 16:18:28 -0300

    projeto alura finalizado

--- projetos front-end/aluramidi-curso-arquivos-iniciais/main.js
+++ projetos front-end/aluramidi-curso-arquivos-iniciais/main.js
@@ -2,10 +2,12 @@
 function tocaSom(seletorAudio){
    const elemento = document.querySelector(seletorAudio);
 
-   if(elemento === null ){
-    console.log('Elemento não encontrado');
+  
+
+   if(elemento != null && elemento.localName === 'audio'){
+     elemento.play();
    }else{
-    console.log(elemento.localName);
+    console.log('Elemento não encontrado');
    }
 
 
