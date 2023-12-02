61848f5fc30802ee5a07cd6ac47b53fc3ebb6d35
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2023-12-02 11:31:57 -0300

    update Fokus project

---
+++ projetos front-end/Fokus-projeto-base/main.js
@@ -0,0 +1,16 @@
+//01-Mudar o background pela tag html
+//02-Criar a funcionalidade dos botões
+
+
+
+const manipulandoOBackgroundPelaTagHtml = document.querySelector('html');
+
+const botaoFoco = document.querySelector('.app__card-button--foco');
+
+
+
+
+
+
+
+
