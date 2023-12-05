1fb5b8da0087575692c28a0a15d27ba4496a86f8
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2023-12-05 08:48:56 -0300

    update 08:48

--- projetos front-end/Fokus-projeto-base/main.js
+++ projetos front-end/Fokus-projeto-base/main.js
@@ -13,6 +13,10 @@ const botaoCurto = document.querySelector('.app__card-button--curto');
 
 const botaoLongo = document.querySelector('.app__card-button--longo');
 
+const titulo = document.querySelector('.app__title');
+
+const listaDeBotoes = document.querySelectorAll('.app__card-button');
+
 
 
 
@@ -21,6 +25,7 @@ botaoFoco.addEventListener('click', () =>{
   // manipulaImagem.setAttribute('src', '/imagens/foco.png')
 
   alterarContexto('foco')
+  botaoFoco.classList.add('active')
 
 
 
@@ -37,6 +42,7 @@ botaoCurto.addEventListener('click', function(){
    //tagHtml.setAttribute('data-contexto', 'descanso-curto')
    //manipulaImagem.setAttribute('src', '/imagens/descanso-curto.png')
    alterarContexto('descanso-curto')
+   botaoCurto.classList.add('active')
 }
     
 );
@@ -45,15 +51,53 @@ botaoLongo.addEventListener('click', ()=>{
   // tagHtml.setAttribute('data-contexto', 'descanso-longo')
    //manipulaImagem.setAttribute('src', '/imagens/descanso-longo.png')
    alterarContexto('descanso-longo')
+   botaoLongo.classList.add('active')
 })
 
 //refatorando
 
 function alterarContexto(contexto){
+    listaDeBotoes.forEach(function (contexto){
+        contexto.classList.remove('active')
+        //pesquiser sobres o metodos do classlist
+    })
     tagHtml.setAttribute('data-contexto', contexto)
     manipulaImagem.setAttribute('src', `/imagens/${contexto}.png`)
 
+    switch(contexto){
+        case "foco":
+            titulo.innerHTML=
+                `
+                Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+                `
+            break;
+
+        case"descanso-curto":
+        titulo.innerHTML=
+                ` Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+            break;
+
+        case"descanso-longo":
+        titulo.innerHTML=
+        ` Hora de voltar à superfície.<br>
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+
+            break;
+
+        default:
+            console.log('sem Resultado');
+            break;
+
+    }
+
 }
+
+
+ 
+   
+
 
 
 
