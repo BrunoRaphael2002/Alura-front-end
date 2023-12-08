bb60e7b65e8a6eafd49199fd6b574444b1af4778
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2023-12-07 22:45:36 -0300

    criando a funcionalidade dos formularios

--- projetos front-end/Fokus-projeto-base/script-crud.js
+++ projetos front-end/Fokus-projeto-base/script-crud.js
@@ -4,6 +4,12 @@ const botaoAddTask = document.querySelector('.app__button--add-task');
 
 const formularioAddTask = document.querySelector('.app__form-add-task ');
 
+const textArea = document.querySelector('.app__form-textarea ');
+
+const ListaDeTarefas = [
+    //lista de tarefas vai ser um Array vazio
+]
+
 botaoAddTask.addEventListener('click', ()=>{
     formularioAddTask.classList.toggle('hidden');
     //o metodo toggle é basicamente o if e else
@@ -13,3 +19,14 @@ botaoAddTask.addEventListener('click', ()=>{
 //possivel criar um menu 
 //navbar hambúrguer responsivo
 
+formularioAddTask.addEventListener('submit', (evento)=>{
+    evento.preventDefault()//evita que a pagina seja recarregada(padrão)
+
+
+   // const descricaoDaTarefa = textArea.value /*o .value pega o valor digitado pelo usuario . A constante  descricaoDaTarefa não será mais necessaria pois o objeto tarefa já vai coletar o valor digitado */
+
+    const tarefa = {//criação de um objeto JavaScript]
+        descricao:
+
+    }
+})
