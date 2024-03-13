1335b70e63661875986e8e15f4ece6a2b80b78d0
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2024-03-12 22:39:57 -0300

    estudo sql

---
+++ sql/tabela cliente.sql
@@ -0,0 +1,15 @@
+CREATE TABLE TABELA_CLIENTE3
+(CPF varchar(11),
+NOME varchar(100),
+ENDERECO1 varchar(150),
+ENDERECO02 varchar (150),
+BAIRRO varchar(50),
+CIDADE  varchar(50),
+ESTADO  varchar(50),
+CEP  varchar(8),
+IDADE smallint,
+SEXO varchar(1),
+LIMITE_DE_CREDITO float,
+VOLUME_COMPRA float,
+PRIMEIRA_COMPRA bit(1)
+)
