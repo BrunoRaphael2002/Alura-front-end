fcf5d15b0627267c33ef52abe696bfc356991d39
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2024-03-20 11:35:07 -0300

    instalando styled-components

--- projetos front-end/react-full-stack/src/App.jsx
+++ projetos front-end/react-full-stack/src/App.jsx
@@ -13,3 +13,17 @@ function App() {
 }
 
 export default App;
+
+/*
+1- instalar o react e seus pacotes
+
+2- entender como funciona o jsx
+
+3- criando componentes 
+
+4- desenvolvendo automações front-end com Js
+
+5- tornando o codigo mais simples de fazer manuntenção e de configurar
+
+7- instalando Styled componentes para tornar mais dinamico o React
+*/
