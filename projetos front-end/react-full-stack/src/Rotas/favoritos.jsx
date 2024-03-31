9844519b744b8a0f28e639d33aa73c5e9dabf75b
BrunoRaphael2002 <105222060+BrunoRaphael2002@users.noreply.github.com>
2024-03-30 22:28:24 -0300

    criando Funcionalidade Post e Delete

--- projetos front-end/react-full-stack/src/Rotas/favoritos.jsx
+++ projetos front-end/react-full-stack/src/Rotas/favoritos.jsx
@@ -1,28 +1,32 @@
-import Pesquisa from '../components/Pesquisa';
-import styled from "styled-components";
+import { useEffect , useState} from 'react';
+import styled from 'styled-components'
+import { getFavoritos } from '../Servicos/favoritos';
 
+  const AppContainer = styled.div `
+        width: 100vw;
+        height: 100vh;
+        background-image: linear-gradient(90deg,#002f52 35%,#326589 165%);
+    `
 
-const AppContainerFav = styled.div`
+  function Favoritos() {
+        const [favoritos, setFavoritos] = useState([])
 
-  width: 100vw;
-  height: 100vh;
-  background-image: linear-gradient(90deg, #002F52 35%, #326589);
+        async function fetchFavoritos() {
+            const favoritosDaAPI = await getFavoritos()
+            setFavoritos(favoritosDaAPI)
+        }
 
+        useEffect(() => {
+            fetchFavoritos()
+        }, [])
 
-li{
-  list-style: none;
-}
-`;
+        return (
+            <AppContainer>
+                 {favoritos.map( favorito => (
+                    <p>{favorito.nome}</p>
+                ) )}
+            </AppContainer>
+        );
+    }
 
-function Favoritos() {
-  return (
-    <AppContainerFav>
-   
-  
-     <Pesquisa/>
-   
-    </AppContainerFav>
-  );
-}
-
-export default Favoritos;
+export default Favoritos
